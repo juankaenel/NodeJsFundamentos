43b141f91176e2b84a965934275f535b69aae3b4
Juan Kaenel <kaenel10@gmail.com>
2020-11-05 18:04:39 -0300

    Implementando templates ejs

--- app.js
+++ app.js
@@ -1,51 +1,28 @@
-/* const {frutas, dinero} = require('./frutas')
-
-const cowsay = require("cowsay");
-
-console.log(cowsay.say({
-    text: 'Hola perro qué onda',
-    e: "oO",
-    T: "U"
-}));
-
-// npx simula instalaciones globales para realizar por ejem
-// la instalación de vue cli. en vez de instalar de forma global la simulas
-
-frutas.forEach(fruta=>{
-    console.log(fruta);
-})
-
-console.log(dinero); */
-/* 
-const http = require('http');
-const server = http.createServer((solicitud,respuesta)=>{
-// respuesta
-    respuesta.end('Estoy respondiendo a tu solicitud v3');
-})
-
-const puerto = 3000;
-// solicitud
-server.listen(puerto, () => {
-    console.log('Escuchando solicitudes');
-}) */
-
-/* Implementando express */
 const express = require('express');
 const app = express();
 
 const port = 3000;
 
-// Middlewares
-app.use(express.static(__dirname + "/public")) //__dirname => ruta dinámica, de la app si la subo al hosting debe tomar la nueva ruta. Por eso usamos ruta dinámica.
-app.use((req,res,next)=>{
-    res.status(404).sendFile(__dirname + "/public/404.html");
+// Motor de plantilla
+app.set('view engine', 'ejs');
+// Ubicacion de plantillas
+app.set('views', __dirname + '/views')
+//Archivos estáticos
+app.use(express.static(__dirname + "/public"))
+
+// Rutitas
+app.get("/", (req, res) => {
+    res.render("index", { titulo: "inicio EJS" });
+  });
+  
+app.get("/nosotros", (req, res) => {
+  res.render("nosotros", { titulo: "Nosotros EJS" });
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { titulo: "Página 404" });
 })
 
-app.get('/', (req,res)=>{
-    console.log(__dirname);
-    res.send('Mi respuesta desde express');
-})
-
-app.listen(port, ()=>{
-    console.log(`Servidor a su servicio ${port}`);
-})
+app.listen(port, () => {
+    console.log('Servidor en el puerto ', port);
+});
